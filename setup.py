e75fb54bdff2f64ba103a7585a399b806c04096c
Tony ElHabr <anthonyelhabr@gmail.com>
2020-10-10 05:31:19 -0500

    Removed added comment

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 
-# Encoding error fix on Windows: https://stackoverflow.com/questions/49640513/unicodedecodeerror-charmap-codec-cant-decode-byte-0x9d-in-position-x-charac
 setup(
     name='AnalyticsHandbook',
     version='0.2dev',

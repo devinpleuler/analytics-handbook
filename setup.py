13c7382a60adfe44bd10cde0cb773a6092ddeafe
Tony ElHabr <anthonyelhabr@gmail.com>
2020-10-10 05:28:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
+# Encoding error fix on Windows: https://stackoverflow.com/questions/49640513/unicodedecodeerror-charmap-codec-cant-decode-byte-0x9d-in-position-x-charac
 setup(
     name='AnalyticsHandbook',
     version='0.2dev',
     packages=['soccerutils', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf8').read(),
 )

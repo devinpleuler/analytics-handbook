0ecdf10fecaade00ec4a5b453ed99fb02e5ce0d1
Devin Pleuler <devinpleuler@gmail.com>
2023-02-06 15:13:23 -0500

    version2 init (#17)

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='AnalyticsHandbook',
-    version='0.2dev',
-    packages=['soccerutils', ],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md', encoding='utf8').read(),
-)
